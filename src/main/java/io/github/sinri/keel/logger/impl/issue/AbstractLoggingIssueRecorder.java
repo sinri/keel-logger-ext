801ebcc824b85ad85ae71f456240ff4961acff9c
sinri <e.joshua.s.e@gmail.com>
2025-11-12 10:36:08 +0800

    Add `AbstractLoggingIssueRecorder` as a new abstract base class for logging issue recorders to enhance structure and modularity.
    Signed-off-by: sinri <e.joshua.s.e@gmail.com>

---
+++ src/main/java/io/github/sinri/keel/logger/impl/issue/AbstractLoggingIssueRecorder.java
@@ -0,0 +1,16 @@
+package io.github.sinri.keel.logger.impl.issue;
+
+import io.github.sinri.keel.logger.api.issue.IssueRecord;
+import io.github.sinri.keel.logger.api.issue.LoggingIssueRecorder;
+import io.github.sinri.keel.logger.api.record.LoggingRecord;
+
+import javax.annotation.Nonnull;
+
+public abstract class AbstractLoggingIssueRecorder<T extends IssueRecord<T>>
+        extends AbstractIssueRecorder<T, LoggingRecord>
+        implements LoggingIssueRecorder<T> {
+    public AbstractLoggingIssueRecorder(@Nonnull String topic) {
+        super(topic);
+    }
+
+}
